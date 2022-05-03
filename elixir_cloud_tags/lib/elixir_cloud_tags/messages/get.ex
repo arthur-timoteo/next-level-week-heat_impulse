b88af0394c7d2f86533dff6ee1760a0bfc333d62
Arthur <arthur.timoteo@gmail.com>
2022-05-02 23:22:47 -0300

    Stage 4 - Get module created

---
+++ elixir_cloud_tags/lib/elixir_cloud_tags/messages/get.ex
@@ -0,0 +1,11 @@
+defmodule ElixirCloudTags.Message.Get do
+  import Ecto.Query
+
+  alias ElixirCloudTags.{Message, Repo}
+
+  def today_messages() do
+    today = Date.utc_today()
+    query = from message in Message, where: type(message.inserted_at, :date) == ^today
+    Repo.all(query)
+  end
+end
